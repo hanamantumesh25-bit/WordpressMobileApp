c00770c4ad1bafa46190ce2aeff0b5412a092a26
hanamantumesh25-bit <hanamantumesh.cs25@bmsce.ac.in>
2026-05-25 23:03:24 +0530

    Remove Redux Provider from MainApp component for simplified structure

--- App.tsx
+++ App.tsx
@@ -1,11 +1,5 @@
-import { Provider } from "react-redux";
-
 import App from "./src";
-import store from "./src/services/storage/store";
+
 export default function MainApp() {
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+  return <App />;
 }
