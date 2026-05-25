9c659bcdfe3efd3497a8a014c2a363d7ab59576b
hanamantumesh25-bit <hanamantumesh.cs25@bmsce.ac.in>
2026-05-25 22:54:59 +0530

    Refactor reducers and store: streamline RootState definition and remove unused imports

--- src/services/storage/reducers/index.ts
+++ src/services/storage/reducers/index.ts
@@ -1,17 +1,12 @@
 import { combineReducers } from "redux";
 
 import postsReducer from "./posts";
-import { PostsState } from "./types";
-
-// Define the type for the combined state
-export interface RootState {
-  posts: PostsState;
-  // Add other slices of state as needed
-}
 
 const rootReducer = combineReducers({
   posts: postsReducer,
   // Add other slices of state as needed
 });
+
+export type RootState = ReturnType<typeof rootReducer>;
 
 export default rootReducer;
--- src/services/storage/store.ts
+++ src/services/storage/store.ts
@@ -1,13 +1,11 @@
 import rootReducer, { RootState } from "@storage/reducers";
-import { createStore, applyMiddleware, Store } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { thunk, ThunkDispatch } from "redux-thunk";
-
-// Define the store type
-export type AppStore = Store<RootState>;
 
 // Define the dispatch type
 export type AppDispatch = ThunkDispatch<RootState, void, any>;
 
-const store: AppStore = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk));
+export type AppStore = typeof store;
 
 export default store;
